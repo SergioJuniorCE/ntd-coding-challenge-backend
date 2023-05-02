b967ea8754dc8ec10696a25b967506e4494d5ae2
Sergio Junior Castro Egurrolar <sergiojuniorce@gmail.com>
2023-05-02 16:10:29 -0700

    updated hosts

--- backend/settings.py
+++ backend/settings.py
@@ -30,9 +30,9 @@ SECRET_KEY = 'django-insecure-_ac1vzpq1b6z=48@j5i7v0=)8azx#@nt&_m+u@)d*m-h28rss5
 DEBUG = True
 
 ALLOWED_HOSTS = ['localhost', '127.0.0.1',
-                 'http://localhost:5173', '310b-158-101-24-137.ngrok-free.app']
+                 'http://localhost:5173', 'https://310b-158-101-24-137.ngrok-free.app']
 
-CSRF_TRUSTED_ORIGINS = ['http://localhost:5173']
+CSRF_TRUSTED_ORIGINS = ['http://localhost:5173', 'https://310b-158-101-24-137.ngrok-free.app']
 
 # CORS Settings
 CORS_ORIGIN_ALLOW_ALL = True
@@ -41,7 +41,8 @@ CORS_ALLOWED_ORIGINS = [
     'http://localhost',
     'http://127.0.0.1',
     'http://0.0.0.0',
-    'http://localhost:5173'
+    'http://localhost:5173',
+    'https://310b-158-101-24-137.ngrok-free.app'
 ]
 
 CORS_ALLOW_CREDENTIALS = True
