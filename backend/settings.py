89cc743bf1d6426e0b30c01b8b1e7d6202129738
Sergio Junior Castro Egurrolar <sergiojuniorce@gmail.com>
2023-05-02 17:35:17 -0700

    possible fix for prod auth

--- backend/settings.py
+++ backend/settings.py
@@ -87,6 +87,7 @@ REST_FRAMEWORK = {
         'rest_framework.permissions.AllowAny'
     ],
     'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.BasicAuthentication',
         'rest_framework.authentication.SessionAuthentication',
     ],
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
